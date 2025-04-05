6f3e023f34d4103ca0977da6d85fd073913131c9
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2025-04-05 22:45:13 +0100

    prints exactly with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly with proper grammar, but the outcome is a piece of art.
+ * main - prints exactly with proper grammar, but the outcome is a piece of art
  * followed by a new line
  * Your program should compile without warning when using the -Wall gcc option
  * Return: Your program should return 0
