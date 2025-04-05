b0e6a0f61ba295f7cb114159591efaa238cae7bc
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2025-04-05 22:42:53 +0100

    prints exactly with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly with proper grammar, but the outcome is a piece of art.
+ * followed by a new line
+ * Your program should compile without warning when using the -Wall gcc option
+ * Return: Your program should return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
