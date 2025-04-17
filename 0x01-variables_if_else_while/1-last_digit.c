cdc413291e47e8ce8a0e30a0b1bf95baba5498d9
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2025-04-17 16:43:44 +0100

    Print the last digit of the number stored in the variable n.(fix betty errors and warnings

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of, %d is %d and is greater than 5", n, n % 10);
@@ -23,5 +23,5 @@ int main(void)
 	{
 		printf("Last digit of, %d is %d and is less than 6 and not 0", n, n % 10);
 	}
-        return (0);
+	return (0);
 } 
