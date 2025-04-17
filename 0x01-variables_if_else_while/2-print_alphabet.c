f0a05ca1cf3187bf5cc649a3f18ad71301580928
root <ayooluwaoloyede71@gmail.com>
2025-04-17 09:40:44 -0700

    Prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ * Return: Always return 0 if success
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
