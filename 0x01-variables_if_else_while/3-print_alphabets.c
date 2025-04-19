31df5bf947684f24041dc4513f3e67284348def3
root <ayooluwaoloyede71@gmail.com>
2025-04-19 05:15:41 -0700

    Prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
+ * You can only use the putchar function.
+ * All your code should be in the main function.
+ * You can only use putchar three times in your code.
+ * Return: Alway return (0) if success.
+ */
 
 int main(void)
 {
@@ -16,5 +24,5 @@ int main(void)
 		b++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ * Print all the letters except q and e
+ * You can only use the putchar function.
+ * All your code should be in the main function.
+ * You can only use putchar twice in your code.
+ * Return: Always return (0) if sucess.
+ */
 int main(void)
 {
 	char a = 'a';
@@ -11,5 +18,5 @@ int main(void)
 		a++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
