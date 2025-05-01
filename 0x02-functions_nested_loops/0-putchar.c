c122fde65c620d1d1792a04de853df41cc3e4816
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2025-05-01 15:58:36 +0100

    prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: Always return 0, is sucess
+ */
+
+int main(void)
+{
+	char a[] = "_putchar";
+	int i = 0;
+	while (a[i] != '\0')
+	{
+		_putchar(a[i]);
+		i++;
+	}
+	_putchar('\n');
+}
