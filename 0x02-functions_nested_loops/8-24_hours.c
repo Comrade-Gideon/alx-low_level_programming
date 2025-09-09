fa249f51ca5cde38b883370cf7791d356c4c9077
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2025-09-09 13:32:36 +0100

    Print every minute of the day of Jack Bauer, starting frim 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,24 +16,28 @@ void jack_bauer(void)
 		hour2 = 0;
 		while (hour2 <= 9)
 		{
-			minute1 = 0;
-			while (minute1 <= 5)
+			if ((hour1 <= 1 && hour2 <= 9) || (hour1 <=2 && hour2 <= 3))
 			{
-				minute2 = 0;
-				while (minute2 <= 9)
+				minute1 = 0;
+				while (minute1 <= 5)
 				{
-					_putchar(hour1 + '0');
-					_putchar(hour2 + '0');
-					_putchar(':');
-					_putchar(minute1	+ '0');
-					_putchar(minute2 + '0');
-					_putchar('\n');
-					minute2++;
+					minute2 = 0;
+					while (minute2 <= 9)
+					{
+						_putchar(hour1 + '0');
+						_putchar(hour2 + '0');
+						_putchar(':');
+						_putchar(minute1	+ '0');
+						_putchar(minute2 + '0');
+						_putchar('\n');
+						minute2++;
+					}
+					minute1++;
 				}
-				minute1++;
+				hour2++;
 			}
-			hour2++;
-		}hour1++;
+		}
+		hour1++;
         }
         return;
 }
